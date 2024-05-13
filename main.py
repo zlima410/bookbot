1e38b49ed487d71ffe4e040bd113050b10aa656a
zlima410 <endercraft68@gmail.com>
2024-05-12 23:20:30 -0400

    Create function to get number of words in the book and a function to create a dictionary of all of the characters and count of occurences

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+    print(f"--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_char_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
