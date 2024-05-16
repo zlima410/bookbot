3c6eb87bc039d706104a6f608241be5cf2ac3f63
zlima410 <endercraft68@gmail.com>
2024-05-15 22:46:36 -0400

    Fix character dictionary to only accept letters and create sort_dict function

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-def main():
+if __name__ == "__main__":
+    def get_num_words(text):
+        words = text.split()
+        return len(words)
+
+    def get_char_dict(text):
+        chars = {}
+        for c in text:
+            if c.isalpha():
+                lowered = c.lower()
+                if lowered in chars:
+                    chars[lowered] += 1
+                else:
+                    chars[lowered] = 1
+        return chars
+    
+    def sort_dict(d):
+        sorted_dict = dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
+        for item in sorted_dict.items():
+            print(f"The '{item[0]}' character was found {item[1]} times")
+        return ""
+
+    def get_book_text(path):
+        with open(path) as f:
+            return f.read()
+        
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    letter_dict = get_char_dict(text)
     print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document\n")
+    print(f"{sort_dict(letter_dict)}")
     print(f"--- End report ---")
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
-def get_char_dict(text):
-    chars = {}
-    for c in text:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
-        else:
-            chars[lowered] = 1
-    return chars
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-
-main()
